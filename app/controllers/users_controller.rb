033f2ed125983795400863acac374332f06d0433
EvonneNgXinYi <evonne1997@gmail.com>
2020-08-25 17:54:23 +0800

    Chapter 8 - Basic Login

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -9,8 +9,8 @@ class UsersController < ApplicationController
 
   def create
     @user = User.new user_params
-
     if @user.save
+      log_in @user
       flash[:success] = t ".success_mess_create"
       redirect_to @user
     else
